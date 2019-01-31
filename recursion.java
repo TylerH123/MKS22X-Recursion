299b176b760ed4fd32cd0ca579bf9eee5de9b812
Tyler Huang <thuang00@stuy.edu>
2019-01-31 18:25:59 -0500

    tried doing fib with 3 parameters

--- recursion.java
+++ recursion.java
@@ -10,6 +10,17 @@ public class recursion{
   public static double squareRoot(double n){
     return squareRoot(n,1);
   }
+  public static int fib(int n, int partial1, int partial2){
+    if (n == 0){
+      return 0;
+    }
+    else{
+      return fib(n-1,partial+partial2,)
+    }
+  }
+  public static int fib(int n){
+    return fib(n,0,1);
+  }
   public static void main(String[] args){
     //System.out.println(squareRoot(100));
     //System.out.println(squareRoot(25));
