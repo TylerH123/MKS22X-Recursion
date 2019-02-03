75573e95becbdada8f480a168e6a82a14bba0c87
Tyler Huang <thuang00@stuy.edu>
2019-02-02 21:11:06 -0500

    tried making it similar to canMakeSum

--- recursion.java
+++ recursion.java
@@ -23,18 +23,26 @@ public class recursion{
   public static int fib(int n){
     return fib(n,1,0);
   }
-  public static ArrayList<Integer> makeAllSums(int n, int g){
+  public static ArrayList<Integer> makeAllSums(int n, int g, int size){
     ArrayList<Integer> L = new ArrayList<Integer>();
+    if (L.size() == Math.pow(2, size)){
+      return L;
+    }
     if (n == 0){
       L.add(g);
-      return L;
     }
     else{
       return makeAllSums(n-1,g+n);
     }
+    if (n == 0){
+      L.add(g);
+    }
+    else{
+      return makeAllSums(n-1,g);
+    }
   }
   public static ArrayList<Integer> makeAllSums(int n){
-    return makeAllSums(n,0);
+    return makeAllSums(n,0,n.size());
   }
   public static void main(String[] args){
     //System.out.println(squareRoot(100));
